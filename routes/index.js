c3130839d5f2838ece97f31a902fed86b5f2fc99
lhafkamp <luukhafkamp@gmail.com>
2017-05-30 18:18:39 +0200

    :hammer: added a storeController

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const router = express.Router()
 const indexController = require('../controllers/indexController');
 const mainController = require('../controllers/mainController');
+const scoreController = require('../controllers/scoreController');
 
 router.get('/', indexController.homePage);
 router.get('/success', indexController.authSuccess);
 
 router.get('/main', mainController.mainPage);
-router.post('/main/score/', mainController.form);
+
+router.get('/score', scoreController.scorePage);
+router.post('/score/result', scoreController.form);
 
 module.exports = router;
