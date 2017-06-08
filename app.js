683e69986329a29f1d309f71d3e5ff37ceef7a99
lhafkamp <luukhafkamp@gmail.com>
2017-06-08 23:07:56 +0200

    test

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const routes = require('./routes/index');
 const app = express();
 
 // get the public files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
