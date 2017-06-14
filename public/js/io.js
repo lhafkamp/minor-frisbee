2e876807b42a192dfb5768d04143b41608f40b46
Luuk Hafkamp <luukhafkamp@gmail.com>
2017-06-14 15:54:11 +0200

    :twisted_rightwards_arrows: Feature/score effects (#10)
    * :sparkles: :lipstick: added some sweet animations to the score updates
    
    * :sparkles: :construction: two progress options
    
    - still have to check which progress option is best

--- public/js/io.js
+++ public/js/io.js
@@ -7,28 +7,51 @@ const numbers = document.querySelectorAll('#score p');
 const leftPercentage = document.querySelector('.leftPrc');
 const rightPercentage = document.querySelector('.rightPrc');
 
+// get the unique game number from the URL
 const location = window.location.href;
 const locationIndex = location.indexOf('/score/');
 const params = location.slice(locationIndex + 7);
 
+// create socket room
 socket.emit('create', params);
 
 socket.on('startVoting', () => {
+	// remove eventual scoreUpdate classes
+	numbers[0].classList.remove('scoreUpdate');
+	numbers[2].classList.remove('scoreUpdate');
+
+	// enable voting options
 	votes.forEach(vote => vote.disabled = false);
 });
 
 socket.on('endVoting', () => {
+	// disable voting options
 	votes.forEach(vote => vote.disabled = true);
 });
 
 socket.on('timeStarted', counter => {
-	timer.innerHTML = counter;
+	// small logic for a better visual experience
+	// let visual = 1;
+	// if (counter < 14) {
+	// 	visual = 0;
+	// }
+	// timer.value = counter + visual;
+
+	// TODO test if this is a better progress option
+	document.querySelector('.test span').classList.add('countdown');
+	if (counter < 1) {
+		document.querySelector('.test span').classList.remove('countdown');
+	}
 });
 
 socket.on('leftVoteResult', score => {
+	// set score
 	numbers[0].innerHTML = score;
 	leftPercentage.innerHTML = 0;
 	rightPercentage.innerHTML = 0;
+
+	// add score update animation and remove percentage colors
+	numbers[0].classList.add('scoreUpdate');
 	leftPercentage.classList.remove('green');
 });
 
@@ -36,23 +59,32 @@ socket.on('rightVoteResult', score => {
 	numbers[2].innerHTML = score;
 	leftPercentage.innerHTML = 0;
 	rightPercentage.innerHTML = 0;
+
+	numbers[2].classList.add('scoreUpdate');
 	rightPercentage.classList.remove('green');
 });
 
 socket.on('percentage', obj => {
 	leftPercentage.innerHTML = obj.leftPercentage;
 	rightPercentage.innerHTML = obj.rightPercentage;
-	if (obj.leftPercentage >= 50) {
+
+	// change percentage box color based on the percentage
+	if (obj.rightPercentage >= 50 && obj.leftPercentage >= 50) {
 		leftPercentage.classList.add('green');
+		rightPercentage.classList.add('green');
 	} else if (obj.rightPercentage >= 50) {
 		rightPercentage.classList.add('green');
+	} else if (obj.leftPercentage >= 50) {
+		leftPercentage.classList.add('green');
 	}
 });
 
+// start time event on click
 function sendEvent() {
 	socket.emit('timeEvent');
 }
 
+// send vote to the server
 function sendScore() {
 	if (this === votes[0]) {
 		scoreObj = {
