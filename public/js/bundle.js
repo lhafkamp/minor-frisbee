fd15d3ce38bb8779f85c94407fa9c93118833529
lhafkamp <luukhafkamp@gmail.com>
2017-06-19 10:58:09 +0200

    :lipstick: score submit styling + functionality

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -6527,19 +6527,17 @@ const done = document.querySelector('.done');
 const form = document.querySelector('form');
 const inputs = document.querySelectorAll('form input');
 const numbers = document.querySelectorAll('#score p');
-
-// start out with 0
-// let leftNumber = 0;
-// let rightNumber = 0;
-// numbers[0].innerHTML = leftNumber;
-// numbers[2].innerHTML = rightNumber;
+const scoreContent = document.querySelector('container');
 
 // fill in form when done
 function fillInForm() {
 	inputs[0].value = numbers[0].textContent;
 	inputs[1].value = numbers[2].textContent;
 	form.classList.remove('hide');
+	scoreContent.classList.add('hide');
+	form.classList.add('swap');
 }
+
 
 done.addEventListener('click', fillInForm);
 
